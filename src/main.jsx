afd2f39633e87bb8eb7ac8c4f0f1b003fe99dfce
linzele <lincolnzele@gmail.com>
2024-10-23 12:39:09 +0800

    finalized v6 policy --lincoln

--- src/main.jsx
+++ src/main.jsx
@@ -24,6 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/reviews" element={<ReviewsPage/>}/>
         <Route path="/storyGenres" element={<StoryGenres/>}/>
         <Route path="/company-profile" element={<CompanyProfile/>}></Route>
+        <Route path="/PrivacyCode" element={<PrivacyCode />} />
       </Routes>
     </Router>
     </AuthProvider>
